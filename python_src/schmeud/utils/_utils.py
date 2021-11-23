fc7e280b677f9dd20e268fe8434a4eb990d75883
Ian Graham <irgraham1@gmail.com>
2021-11-23 16:22:59 -0500

    change string parsing

--- python_src/schmeud/utils/_utils.py
+++ python_src/schmeud/utils/_utils.py
@@ -3,5 +3,5 @@
 def tail(path):
     return path.split('/')[-1]
 
-def parse_flag_from_string(string, flag):
-    return string.split(f'_{flag}')[1].split("_")[0]
+def parse_flag_from_string(string, flag, end="_"):
+    return string.split(flag)[1].split(end)[0]
